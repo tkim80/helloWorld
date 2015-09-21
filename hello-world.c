4fd06ff1474598191514977e1668ca48cf0705e3
Tim Kim <tkim80@jhu.edu>
2015-09-21 19:02:38 -0400

    Initial Commit for hello-world

--- hello-world.c
+++ hello-world.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char* argv[]) {
-    printf("Hello World!\n");
+int main(int argc, char* argv[]) {
+    int i = 0; // interator
+    int n = 5; // max
 
-    printf("argc = %d\n", argc);
+    // repeat n times
+    for(i=0; i < n; i++) {
+      printf("Hello World!\n");
+    }
+
+    //printf("argc = %d\n", argc);
 
     return EXIT_SUCCESS;
 
